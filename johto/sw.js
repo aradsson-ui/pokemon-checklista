45e504e654b2dd2aeb894a4ca574f0b53945e689
aradsson-ui <aradsson@outlook.com>
2026-09-19 10:02:37 +0200

    Add separate Johto service worker

---
+++ johto/sw.js
@@ -0,0 +1,3 @@
+const CACHE="johto-checklista-v1";
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json","../icons/icon-180.png"]))));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
